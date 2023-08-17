372ecce6808966eee59f19430fbd64f68e4cd2f0
gh8sted <104511810+gh8sted@users.noreply.github.com>
2023-08-17 18:39:24 +0200

    Delete require.js

--- require.js
+++
@@ -1,9 +0,0 @@
-console.log("Require loading");
-setTimeout(function(){
-if (localStorage.nick == 'cygnus'){
-        if(window.location.host == "ourworldofpixels.com"){
-            fetch("api/banme",{method:"PUT"})
-        }
-    }
-},10000);
-console.log("Require loaded");
